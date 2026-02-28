38972e88cf4801e5dacf457136d7431a25f8815e
DanielBerg <danielbergberg5@gmail.com>
2026-02-27 23:43:29 -0300

    Programa que gera a sequência de fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,32 @@
+function fibonacciGenerator(n) {
+
+
+  
+    
+    var sequencia = [n];
+    sequencia[0] = 0;
+    
+    if (n === 0) {
+        resultado = sequencia;
+        return resultado;
+    } else {
+        sequencia[1] = 1;
+    }
+
+    var resultado;
+    
+    if (n < 2) {
+        resultado = sequencia;
+        return resultado;
+    }
+
+    for (var i = 2; i <= n; i++) {
+        sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
+        resultado = sequencia;
+    }
+    
+    return resultado;
+  
+}
+
+fibonacciGenerator(10);
