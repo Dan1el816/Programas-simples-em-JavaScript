6021152743a3524b1ef477f25c8714e31d9f5de2
DanielBerg <danielbergberg5@gmail.com>
2026-02-28 08:07:33 -0300

    mudando lógica

--- fibonacci.js
+++ fibonacci.js
@@ -1,35 +1,27 @@
 function fibonacciGenerator(n) {
+  var sequencia = [];
+  var resultado;
 
-
-  
-    
-    var sequencia = [0];
-    sequencia[0] = 0;
-    
-
-    var resultado;
-
-    if (n === 0) {
-         resultado = sequencia;
-        return resultado;
-    } else {
-        sequencia[1] = 1;
-    }
-
-    
-    
-    if (n === 1) {
-        resultado = sequencia;
-        return resultado;
-    }
-
-    for (var i = 2; i <= n; i++) {
-        sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
-        resultado = sequencia;
-    }
-    
+  if (n === 0) {
+    resultado = sequencia;
     return resultado;
-  
+  } else {
+    sequencia[1] = 1;
+    sequencia[0] = 0;
+  }
+
+
+  if (n === 1) {
+    resultado = sequencia[0];
+    return resultado;
+  }
+
+  for (var i = 2; i <= n; i++) {
+    sequencia[i] = sequencia[i - 1] + sequencia[i - 2];
+    resultado = sequencia;
+  }
+
+  return resultado;
 }
 
 fibonacciGenerator(10);
